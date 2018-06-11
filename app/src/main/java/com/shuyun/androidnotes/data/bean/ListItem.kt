3e2e65d1901e2c2218871bfc91f654931bb2d902
shuyun <shuyungogogo@hotmail.com>
2018-06-11 21:16:03 +0800

    commit

---
+++ app/src/main/java/com/shuyun/androidnotes/data/bean/ListItem.kt
@@ -0,0 +1,9 @@
+package com.shuyun.androidnotes.data.bean
+
+/**
+ * item data for ListView or RecyclerView
+ * @author shuyun
+ * @created on 2018/6/7 0007 17:45
+ * @changed on 2018/6/7 0007 17:45
+ */
+data class ListItem(var title: String, var content: String)
