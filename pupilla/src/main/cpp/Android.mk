97456ff92490edf5aaaeb0e20047c93482e08577
Shuyun <shuyungogogo@hotmail.com>
2018-06-12 18:47:00 +0800

    commit

--- pupilla/src/main/cpp/Android.mk
+++ pupilla/src/main/cpp/Android.mk
@@ -2,5 +2,5 @@ PROJ_PATH	:= $(call my-dir)
 include $(CLEAR_VARS)
 include $(PROJ_PATH)/pupilla/Android.mk
 include $(PROJ_PATH)/libjpeg-turbo-1.5.0/Android.mk
-include $(PROJ_PATH)/libusb/android/jni/Android.mk
+include $(PROJ_PATH)/libusb1/android/jni/Android.mk
 include $(PROJ_PATH)/libuvc1/android/jni/Android.mk
