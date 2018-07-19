9c4583e01c50b61bfba845e537b2606c935da231
Shuyun <shuyungogogo@hotmail.com>
2018-07-19 16:19:45 +0800

    commit

---
+++ app/src/main/java/com/shuyun/androidnotes/activity/Test.kt
@@ -0,0 +1,13 @@
+package com.shuyun.androidnotes.activity
+
+/**
+ * $desc$
+ * @author shuyun
+ * @created on 2018/7/19 0019 16:18
+ * @changed on 2018/7/19 0019 16:18
+ */
+class Test {
+
+    //Check if the SecretExecutor change the file
+
+}
