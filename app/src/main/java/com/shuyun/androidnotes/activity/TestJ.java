6ada7c10eaf591dcb88c7986b29c177b50cb59ed
Shuyun <shuyungogogo@hotmail.com>
2018-07-19 16:21:54 +0800

    commit

---
+++ app/src/main/java/com/shuyun/androidnotes/activity/TestJ.java
@@ -0,0 +1,12 @@
+package com.shuyun.androidnotes.activity;
+
+/**
+ * $desc$
+ *
+ * @author shuyun
+ * @created on 2018/7/19 0019 16:21
+ * @changed on 2018/7/19 0019 16:21
+ */
+public class TestJ {
+    //check if
+}
