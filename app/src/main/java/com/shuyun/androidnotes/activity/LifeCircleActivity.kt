4f41a91de4e1211b720c1b9cbe4a6eb69a322584
shuyun <shuyungogogo@hotmail.com>
2019-01-05 17:59:24 +0800

    add an example

--- app/src/main/java/com/shuyun/androidnotes/activity/LifeCircleActivity.kt
+++ app/src/main/java/com/shuyun/androidnotes/activity/LifeCircleActivity.kt
@@ -1,8 +1,15 @@
 package com.shuyun.androidnotes.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import com.shuyun.androidnotes.utils.Log
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 /**
  * $desc$
@@ -15,46 +22,55 @@ open class LifeCircleActivity: AppCompatActivity() {
     lateinit var user: String
     lateinit var password: String
 
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.Companion.e("onCreate")
         user = "shuyun"
         password = "123"
+        Observable.timer(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    AlertDialog.Builder(this)
+                            .setMessage("MSG")
+                            .create().show()
+                }
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.Companion.e("onRestart")
+        Log.e("onRestart")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.Companion.e("onStart")
+        Log.e("onStart")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.Companion.e("onResume")
+        Log.e("onResume")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.Companion.e("onPause")
+        Log.e("onPause")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.Companion.e("onStop")
+        Log.e("onStop")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.Companion.e("onDestroy")
+        Log.e("onDestroy")
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        Log.Companion.e("onSaveInstanceState")
+        Log.e("onSaveInstanceState "+outState?.toString())
 
     }
 
