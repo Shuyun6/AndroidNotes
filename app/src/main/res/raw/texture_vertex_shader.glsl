33804b38954d07bc97ae5b06b446654e8a2f280a
Shuyun <shuyungogogo@hotmail.com>
2019-03-20 16:55:02 +0800

    add some texture shader

---
+++ app/src/main/res/raw/texture_vertex_shader.glsl
@@ -0,0 +1,9 @@
+attribute vec4 a_Position;
+attribute vec2 a_TextureCoordinates;
+uniform mat4 u_Matrix;
+varying vec2 v_TextureCoordinates;
+
+void main() {
+    v_TextureCoordinates = a_TextureCoordinates;
+    gl_Position = a_Position;
+}
